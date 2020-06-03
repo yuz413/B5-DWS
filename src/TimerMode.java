be63afacdbd1323821fbc4d97e7c2ae9fd45a3bf
yuz413 <yujn413@naver.com>
2020-06-03 18:25:15 +0900

    xml 문제

--- src/TimerMode.java
+++ src/TimerMode.java
@@ -9,6 +9,22 @@ public class TimerMode {
     private Thread timer_runner;
     private Buzzer buzzer;
 
+    TimerMode() {
+        setted_time = new Time();
+        timer_runner = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    while(!checkTimerZero()) {
+                        Thread.sleep(1000);
+                        decreaseTimer();
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
     public void initTimerMode() {
         decreaseTimer();
     }
@@ -19,9 +35,15 @@ public class TimerMode {
     private void increaseTimeMinute() {}
     private void increaseTimeSeconds() {}
     private void loadSettingTime() {}
-    //    private boolean checkTimerZero() {
-//    }
+    private boolean checkTimerZero() {
+        if(setted_time.getMinute() == 0 && setted_time.getSeconds() == 0) {
+            return true;
+        }
+        return false;
+    }
     private void decreaseTimer() {
+        int curr_seconds = setted_time.getSeconds();
+        int curr_minute = setted_time.getMinute();
     }
     private void mappingTimerState() {}
     private void mappingTimerRunning() {}
