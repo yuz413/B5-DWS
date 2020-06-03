18786c66215cef7f12aff9f8eeb562121ff938ce
purum5548 <48601176+purum5548@users.noreply.github.com>
2020-06-03 17:05:26 +0900

    TimeAdded
    --

--- src/Segment.java
+++ src/Segment.java
@@ -1,8 +1,14 @@
 import java.awt.*;
 
 public class Segment {
+    private Color text_color, background_color;
+
+
+
     public void SetSegmentUpper(){}
     public void SetSegmentLower(){}
     public void SetBackgroundColor(Color color){}
     public void SetTextColor(Color color){}
+    public String getSegmentUpper(){return null;}
+    public String getSegmentLower(){return null;}
 }
--- src/TimeManager.java
+++ src/TimeManager.java
@@ -1,3 +1,27 @@
-public class TimeManager {
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
+public class TimeManager {
+    private static TimeManager instance = null;
+    private LocalDateTime local_date_time;
+
+    private TimeManager(){ }
+
+    public static TimeManager getInstance(){
+        if(instance == null){
+            instance = new TimeManager();
+        }
+        return instance;
+    }
+
+    public Time getCurrentTime(){
+        if(TestCurrentTime());
+        return new Time(local_date_time);
+    }
+
+    public Boolean TestCurrentTime(){
+        local_date_time = LocalDateTime.now();
+        return true;
+    }
 }
