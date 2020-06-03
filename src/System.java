ce809a7b6a1dc6dd9cfcdbb728749bab19805c99
purum5548 <48601176+purum5548@users.noreply.github.com>
2020-06-03 17:03:19 +0900

    Time and TimeManager Added

--- src/System.java
+++ src/System.java
@@ -1,3 +1,50 @@
+import java.awt.*;
+
 public class System {
 
+    private Segment segment;
+    private ModeManager mode_manager;
+
+
+    public System(){
+        segment = new Segment();
+        mode_manager = new ModeManager();
+
+    }
+
+    public void pressButtonA(){
+
+    }
+
+    public void pressButtonB(){
+
+    }
+
+    public void pressButtonC(){
+
+    }
+
+    public void pressButtonD(){
+
+    }
+
+    public String getSegmentContentLower(){
+        return segment.getSegmentLower();
+    }
+
+    public String getSegmentContentUpper(){
+        return segment.getSegmentUpper();
+    }
+
+    public Color getTextColor(){
+        return null;
+    }
+
+    public Color getBackGroundColor(){
+        return null;
+    }
+
+    public void initWatch(){
+
+    }
 }
