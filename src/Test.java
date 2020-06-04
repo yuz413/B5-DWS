95744ff03b26cda6e1edf60e30f7d79957daa6a5
purum5548 <48601176+purum5548@users.noreply.github.com>
2020-06-05 00:11:12 +0900

    Font 추가
    폰트 빼먹어서 추가함.......

--- src/Test.java
+++ src/Test.java
@@ -2,20 +2,6 @@ import java.net.URL;
 
 public class Test {
     public static void main(String[] args) {
-        Buzzer buzzer = new Buzzer();
-        buzzer.OnBuzzer();
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        buzzer.OffBuzzer();
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        buzzer.OnBuzzer();
-        buzzer.OnBuzzer();
+
     }
 }
